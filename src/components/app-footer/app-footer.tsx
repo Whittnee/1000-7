d8ff3c74a75c0f8449f38aa20c74dfefc5821ac0
Ayman <aimankinq@gmail.com>
2025-02-03 13:00:58 +0300

    update: updated footer

--- src/components/app-footer/app-footer.tsx
+++ src/components/app-footer/app-footer.tsx
@@ -1,79 +1,64 @@
-import { Link } from "react-router";
+
 import styles from "./app-footer.module.scss";
-import { FaApplePay, FaGithub, FaGooglePay, FaInstagram, FaTwitter } from "react-icons/fa";
+import {
+  FaApplePay,
+  FaGithub,
+  FaGooglePay,
+  FaInstagram,
+} from "react-icons/fa";
 import { RiVisaLine } from "react-icons/ri";
 import { SiMastercard } from "react-icons/si";
+import { FC } from "react";
+import { Separator } from "../ui/separator";
+import { FaXTwitter } from "react-icons/fa6";
 
-export const AppFooter = () => {
+export const AppFooter: FC = () => {
   return (
     <footer className={styles.footer}>
-      <div className={styles.contentWrapper}>
-        <div className={styles.logoWrapper}>
+      <section className={styles.content}>
+        <div className={styles.info}>
           <h3 className={styles.logo}>1000-7</h3>
           <span className={styles.text}>
-            We have clothes that highlight your ghoul style and which you’ll be
-            proud to wear.
+            We have clothes that suits your ghoul style and which you're proud
+            to wear.
           </span>
-          <ul className={styles.ulIcons}>
+          <ul className={styles.icons}>
             <li>
               <a className={styles.link} href="">
-                <FaInstagram className={styles.socialIcon} />
+                <FaInstagram className={styles.icon} />
               </a>
             </li>
             <li>
-              <a
-                className={styles.link}
-                href="https://github.com/Whittnee"
-                target="_blank"
-              >
-                <FaGithub className={styles.socialIcon} />
+              <a className={styles.link} href="https://github.com/Whittnee" target="_blank">
+                <FaGithub className={styles.icon} />
               </a>
             </li>
             <li>
-              <a
-                className={styles.link}
-                href="/"
-                target="_blank"
-              >
-                <FaTwitter className={styles.socialIcon} />
+              <a className={styles.link} href="/" target="_blank">
+                <FaXTwitter className={styles.icon} />
               </a>
             </li>
           </ul>
         </div>
-        <nav className={styles.menu}>
-          <div className={styles.div}>
-            <h4 className={styles.h4}>company</h4>
-            <ul className={styles.ul}>
-              <li>
-                <Link to="/about" className={styles.text}>About</Link>
-              </li>
-              <li>
-                <Link to="/about" className={styles.text}>Career</Link>
-              </li>
-            </ul>
-          </div>
-          <div className={styles.div}>
-            <h4 className={styles.h4}>faq</h4>
-            <ul className={styles.ul}>
-              <li>
-                <Link to="/about" className={styles.text}>Account</Link>
-              </li>
-              <li>
-                <Link to="/about" className={styles.text}>Payment</Link>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </div>
-      <div className={styles.license}>
+      </section>
+      <Separator/>
+      <section className={styles.license}>
         <p className={styles.text}>1000-7 © 2024-2025, All rights reserved.</p>
-        <ul className={styles.paymentIcons}>
-          <li><RiVisaLine className={styles.paymentIcon}/></li>
-          <li><SiMastercard className={styles.paymentIcon}/></li>
-          <li><FaGooglePay className={styles.paymentIcon}/></li>
-          <li><FaApplePay className={styles.paymentIcon}/></li>
+        <ul className={styles.icons}>
+          <li>
+            <RiVisaLine className={styles.paymentIcon} />
+          </li>
+          <li>
+            <SiMastercard className={styles.paymentIcon} />
+          </li>
+          <li>
+            <FaGooglePay className={styles.paymentIcon} />
+          </li>
+          <li>
+            <FaApplePay className={styles.paymentIcon} />
+          </li>
         </ul>
-      </div>
+      </section>
     </footer>
   );
 };
