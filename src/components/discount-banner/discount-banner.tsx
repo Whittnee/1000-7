533554123fd01f79097b6d583e0820d055bc2a22
Ayman <aimankinq@gmail.com>
2025-02-07 08:10:40 +0300

    update: updated discount banner

--- src/components/discount-banner/discount-banner.tsx
+++ src/components/discount-banner/discount-banner.tsx
@@ -1,8 +1,9 @@
 import { IoMdClose } from "react-icons/io";
 import styles from "./discount-banner.module.scss";
-import { useEffect, useState } from "react";
+import { FC, useEffect, useState } from "react";
+import { Link } from "react-router";
 
-export const DiscountBanner = () => {
+export const DiscountBanner: FC = () => {
   const [isVisible, setIsVisible] = useState(true);
   useEffect(() => {
     const hasClosed = localStorage.getItem("hasClosed");
@@ -18,14 +19,16 @@ export const DiscountBanner = () => {
   if (!isVisible) return null;
 
   return (
-    <div className={styles.contentWrapper}>
-      <p className={styles.text}>
-        Sign up and get 30% discount on your first order.
-      </p>
-      <a className={styles.link} href="#">
-        Sign up Now
-      </a>
-      <IoMdClose className={styles.closeIcon} onClick={handleClick} />
-    </div>
+  <div className={styles.discountBanner}>
+    <p className={styles.text}>
+      Sign up and get 30% discount on your first order.
+    </p>
+    <Link to="/sign-up">
+      <p className={styles.link}>Sign up Now</p>
+    </Link>
+    <button className={styles.button}>
+      <IoMdClose type="button" onClick={handleClick} />
+    </button>
+  </div>
   );
 };
